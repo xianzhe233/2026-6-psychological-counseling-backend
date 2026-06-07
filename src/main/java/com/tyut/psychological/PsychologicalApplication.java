9e3574522ceba6697fcaf43e3ddf7a2485b7b72a
1gggllll <2767882448@qq.com>
2026-06-07 19:04:13 +0800

    fix: 修改MapperScan配置以扫描所有mapper包

--- src/main/java/com/tyut/psychological/PsychologicalApplication.java
+++ src/main/java/com/tyut/psychological/PsychologicalApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.tyut.psychological.*.mapper")
+@MapperScan("com.tyut.psychological.**.mapper")
 public class PsychologicalApplication {
 
     public static void main(String[] args) {
